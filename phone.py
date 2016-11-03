d9a366b4f0e6c97d726f95a5abeb0c4d3611ca9f
shivangi <shivangishipra5@gmail.com>
2016-11-03 16:30:42 +0530

    get phone no. from dir

--- phone.py
+++ phone.py
@@ -1,9 +1,22 @@
 import re
+import os
+from os.path import isfile
 
-f = open('0009603','r')
-fileText = f.read()
-tofind = re.compile('\d\d\d\d\d\d\d\d\d\d')
-match = re.findall(tofind,fileText)
-for u in match:
-	print str(u)
+def getPhone(name):
+	f = open(name,'r')
+	fileText = f.read()
+	tofind = re.compile('\d\d\d\d\d\d\d\d\d\d')
+	match = re.findall(tofind,fileText)
+	for u in match:
+		print "+91-" + str(u)
 
+
+directory = '/home/sonal/Desktop/myDir'
+
+def recurse(dirName):
+	for subdir, dirs, files in os.walk(dirName):
+		for file in files:
+		 	path = os.path.join(subdir, file)
+			getPhone(path)
+
+recurse(directory)
